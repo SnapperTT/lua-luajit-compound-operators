2e5d140f06b991cb74ad31b278b90183556ef4eb
Liam Twigger <4939563+SnapperTT@users.noreply.github.com>
2021-11-13 13:36:53 +1000

    Fixed fetch_and_apply_luajit.sh -> look for luajit in github/tags instead of github/releases

--- fetch_and_apply_luajit.sh
+++ fetch_and_apply_luajit.sh
@@ -1,6 +1,6 @@
 mkdir luajit210
 cd luajit210
-LATEST=$(curl -s https://github.com/LuaJIT/LuaJIT/releases | grep -m 1 "tar.gz" | cut -d \" -f4)
+LATEST=$(curl -s https://github.com/LuaJIT/LuaJIT/tags | grep -m 1 "tar.gz" | cut -d \" -f4)
  wget https://github.com/$LATEST
 TARFILE=${LATEST##*/}
 tar -xvf $TARFILE --strip 1
