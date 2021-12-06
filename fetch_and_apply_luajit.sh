87f8e3f7036563d4f3b15d550696073704295272
Liam Twigger <4939563+SnapperTT@users.noreply.github.com>
2021-12-06 15:52:01 +1000

    Changed fetch_and_apply_luajit.sh to make it smarter looking for luajit download

--- fetch_and_apply_luajit.sh
+++ fetch_and_apply_luajit.sh
@@ -1,7 +1,39 @@
 mkdir luajit210
 cd luajit210
-LATEST=$(curl -s https://github.com/LuaJIT/LuaJIT/tags | grep -m 1 "tar.gz" | cut -d \" -f4)
- wget https://github.com/$LATEST
+rm -rf *
+
+fetch_git_release() {
+        local URL_RAW=$(curl -s $1/releases | grep -m 1 "tar.gz")
+        local URL1=$(echo $URL_RAW | cut -d \" -f2)
+        local URL2=$(echo $URL_RAW | cut -d \" -f4)
+	if [[ "$URL1" == *"tar.gz"* ]]; then
+                echo $URL1
+                return 0
+        fi
+        if [[ "$URL2" == *"tar.gz"* ]]; then
+                echo $URL2
+                return 0
+        fi
+        
+        local URL_RAW2=$(curl -s $1/tags | grep -m 1 "tar.gz")
+        local URL3=$(echo $URL_RAW2 | cut -d \" -f2)
+        local URL4=$(echo $URL_RAW2 | cut -d \" -f4)
+        if [[ "$URL3" == *"tar.gz"* ]]; then
+                echo $URL3
+                return 0
+        fi
+        if [[ "$URL4" == *"tar.gz"* ]]; then
+                echo $URL4
+                return 0
+        fi
+        
+        return 1
+        }
+
+
+LATEST=$(fetch_git_release "https://github.com/LuaJIT/LuaJIT")
+echo "Downloading: $LATEST"
+wget https://github.com/$LATEST
 TARFILE=${LATEST##*/}
 tar -xvf $TARFILE --strip 1
 cd src/
